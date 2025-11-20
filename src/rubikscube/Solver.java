9a47a3be45419b61153c954d36ce68417615465a
Citroll <quintinlam8@gmail.com>
2025-11-20 05:33:12 -0800

    implement cube
    ad

--- src/rubikscube/Solver.java
+++ src/rubikscube/Solver.java
@@ -15,6 +15,8 @@ public class Solver {
         public int index;
     }
 
+    private char cube[][][] = new char[6][3][3];
+
     public static void main(String[] args) {
         System.out.println("number of arguments: " + args.length);
         for (int i = 0; i < args.length; i++) {
