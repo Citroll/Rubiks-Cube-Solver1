511de0eb56053463f22bd41bf0d8371a1cc733df
Citroll <quintinlam8@gmail.com>
2025-11-26 19:36:33 -0800

    Added classes for cubies faces and facelets instead of  dumb 3d array

--- src/rubikscube/Solve.java
+++ src/rubikscube/Solve.java
@@ -6,24 +6,60 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solve {
+    Corner[] corners = new Corner[8];
+    Edge[] edges = new Edge[12];
+    Center[] center = new Center[8];
 
-    private int[] cornerPerm = new int[8];
-    private int[] edgePerm = new int[12];
+    Corner[] solvedCorners = new Corner[8];
+    Edge[] solvedEdges = new Edge[12];
+    Center[] solvedCenter = new Center[8];
 
-    private char[][][] cube = new char[6][3][3];
-    private char[][][] solvedCube = new char[6][3][3];
-    private static final int UP = 0, LEFT = 1, FRONT = 2, RIGHT = 3, BACK = 4, DOWN = 5;
-    private String solved
-            = "   OOO\n"
-            + "   OOO\n"
-            + "   OOO\n"
-            + "GGGWWWBBBYYY\n"
-            + "GGGWWWBBBYYY\n"
-            + "GGGWWWBBBYYY\n"
-            + "   RRR\n"
-            + "   RRR\n"
-            + "   RRR\n";
+    public static class Facelet{
+        char colour;
+        int face;
 
+        public Facelet(char colour, int face){
+            this.colour = colour;
+            this.face = face;
+        }
+    }
+
+    public static class Center{
+        int index;
+        Facelet f;
+
+        public Center(int index, Facelet f){
+            this.index = index;
+            this.f = f;
+        }
+    }
+
+    public static class Edge{
+        int index;
+        int ori;
+        Facelet f1, f2;
+
+        public Edge(int index, int ori, Facelet f1, Facelet f2){
+            this.index = index;
+            this.ori = ori;
+            this.f1 = f1;
+            this.f2 = f2;
+        }
+    }
+
+    public static class Corner{
+        int index;
+        int ori;
+        Facelet f1, f2, f3;
+
+        public Corner(int index, int ori, Facelet f1, Facelet f2, Facelet f3){
+            this.index = index;
+            this.ori = ori;
+            this.f1 = f1;
+            this.f2 = f2;
+            this.f3 = f3;
+        }
+    }
     /*
               UP
         LEFT FRONT RIGHT BACK
