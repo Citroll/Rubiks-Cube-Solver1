52ad572dd0414325b48693f02f0cd18efea37c7a
Citroll <quintinlam8@gmail.com>
2025-12-01 02:18:54 -0800

    implemented the constructors for readfile and solved string

--- src/rubikscube/Solve.java
+++ src/rubikscube/Solve.java
@@ -1,6 +1,6 @@
 package rubikscube;
 
-import java.io.File;
+import java.io.*;
 
 public class Solve {
 
@@ -27,31 +27,30 @@ public class Solve {
     //CORNERS: UFL, URF, UBR, ULB, DLF, DFR, DRB, DBL
     //EDGES: UR, UF, UL, UB, DR, DF, DL, DB, FR, FL, BL, BR
     private static final int[][] CORNERS = {
-        {U,2,0, F,0,0, L,0,2}, //0 UFL
-        {U,2,2, R,0,0, F,0,2}, //1 URF
-        {U,0,2, B,0,0, R,0,2}, //2 UBR
-        {U,0,0, L,0,0, B,0,2}, //3 ULB
-        {D,0,0, L,2,2, F,2,0}, //4 DLF
-        {D,0,2, F,2,2, R,2,0}, //5 DFR
-        {D,2,2, R,2,2, B,2,0}, //6 DRB
-        {D,2,0, B,2,2, L,2,0} //7 DBL
+        {U, 2, 0, F, 0, 0, L, 0, 2}, //0 UFL
+        {U, 2, 2, R, 0, 0, F, 0, 2}, //1 URF
+        {U, 0, 2, B, 0, 0, R, 0, 2}, //2 UBR
+        {U, 0, 0, L, 0, 0, B, 0, 2}, //3 ULB
+        {D, 0, 0, L, 2, 2, F, 2, 0}, //4 DLF
+        {D, 0, 2, F, 2, 2, R, 2, 0}, //5 DFR
+        {D, 2, 2, R, 2, 2, B, 2, 0}, //6 DRB
+        {D, 2, 0, B, 2, 2, L, 2, 0} //7 DBL
     };
 
     private static final int[][] EDGES = {
-        {U,1,2, R,0,1}, //0 UR
-        {U,2,1, F,0,1}, //1 UF
-        {U,1,0, L,0,1}, //2 UL
-        {U,0,1, B,0,1}, //3 UB
-        {D,1,2, R,2,1}, //4 DR
-        {D,0,1, F,2,1}, //5 DF
-        {D,1,0, L,2,1}, //6 DL
-        {D,2,1, B,2,1}, //7 DB
-        {F,1,2, R,1,0}, //8 FR
-        {F,1,0, L,1,2}, //9 FL
-        {B,1,2, L,1,0}, //10 BL
-        {R,1,2, B,1,0}, //11 BR
+        {U, 1, 2, R, 0, 1}, //0 UR
+        {U, 2, 1, F, 0, 1}, //1 UF
+        {U, 1, 0, L, 0, 1}, //2 UL
+        {U, 0, 1, B, 0, 1}, //3 UB
+        {D, 1, 2, R, 2, 1}, //4 DR
+        {D, 0, 1, F, 2, 1}, //5 DF
+        {D, 1, 0, L, 2, 1}, //6 DL
+        {D, 2, 1, B, 2, 1}, //7 DB
+        {F, 1, 2, R, 1, 0}, //8 FR
+        {F, 1, 0, L, 1, 2}, //9 FL
+        {B, 1, 2, L, 1, 0}, //10 BL
+        {R, 1, 2, B, 1, 0}, //11 BR
     };
-
 
     public static class Facelet {
 
@@ -110,12 +109,67 @@ public class Solve {
              DOWN
      */
     public Solve() {
+        char[][][] solvedCube = readString(SOLVED);
     }
 
     public Solve(File file) {
+        char[][][] cube = readFile(file);
+        char[][][] solvedCube = readString(SOLVED);
     }
 
-    private void solvedCube(){
-        
+    private void solvedCube() {
+
+    }
+
+    public char[][][] readFile(File file) {
+        String[] lines = new String[9];
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            for (int i = 0; i < 9; i++) {
+                lines[i] = br.readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return readLines(lines);
+    }
+
+    public char[][][] readString(String string) {
+        String[] lines = string.split("\n");
+        return readLines(lines);
+    }
+
+    public char[][][] readLines(String[] lines) {
+        char[][][] cube = new char[6][3][3];
+
+        //Up
+        for (int i = 0; i < 3; i++) {
+            String line = lines[i];
+            for (int k = 0; k < 3; k++) {
+                cube[U][i][k] = line.charAt(3 + k);
+            }
+        }
+
+        //Left, Front, Right, Back
+        for (int i = 0; i < 3; i++) {
+            String line = lines[3 + i];
+
+            for (int k = 0; k < 3; k++) { //Left
+                cube[L][i][k] = line.charAt(k);
+                cube[F][i][k] = line.charAt(k + 3);
+                cube[R][i][k] = line.charAt(k + 6);
+                cube[B][i][k] = line.charAt(k + 9);
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            String line = lines[6 + i];
+            for (int k = 0; k < 3; k++) {
+                cube[D][i][k] = line.charAt(3 + k);
+            }
+        }
+
+        return cube;
+
     }
 }
